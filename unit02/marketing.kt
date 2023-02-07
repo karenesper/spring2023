453a810b1e3d7b3f1f7f6e59f3fd99fa72cb40d0
Esperanza Macias <123098144+karenesper@users.noreply.github.com>
2023-02-07 14:13:01 -0600

    Create marketing.kt

---
+++ unit02/marketing.kt
@@ -0,0 +1,16 @@
+//Esperanza Macias 
+//February 7, 2023
+fun main() 
+{
+var friendsCount = 500 
+if(friendsCount >  500)
+    {
+        println("Adopt a dog today!")
+    }
+else 
+    {
+    println("Buy Cat Food at 20% Off")
+}
+    
+ 
+}
