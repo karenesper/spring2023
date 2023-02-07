17996595aed1249f41c15cbf96edff1375e71952
Esperanza Macias <123098144+karenesper@users.noreply.github.com>
2023-02-07 14:33:49 -0600

    Create salary.kt

---
+++ unit02/salary.kt
@@ -0,0 +1,20 @@
+//Esperanza Macias 
+//February 7, 2023
+
+fun main() 
+{
+val name = "Esperanza";
+val age = 22;
+val annualPay = 100000.0;
+  
+println("My name is $name, my age is $age and I hope to earn $annualPay per year.")
+    
+ 
+}
+
+
+
+
+
+
+
