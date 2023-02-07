89a53ca94e72903b90b2c63a9671ade4ceeb4f02
Esperanza Macias <123098144+karenesper@users.noreply.github.com>
2023-02-07 14:10:18 -0600

    Update variables.kt

---
+++ unit02/variables.kt
@@ -0,0 +1,13 @@
+// Esperanza Macias 
+// February 07, 2023
+
+fun main() {
+   val myAge = 22;
+   var averageAge = (22.0 + 30) / 2
+   val testNumber = 10
+   val evenOdd = testNumber % 2
+   val answer = (((0 + 1) + 10) * 10) * 1000
+   println(myAge)
+   println(averageAge)
+   println(evenOdd)
+   println(answer)
