5724c97f207fabeeb2ea5d0137e4f1dbc056d949
Esperanza Macias <123098144+karenesper@users.noreply.github.com>
2023-02-07 20:50:51 -0600

    Create star.kt

---
+++ unit02/star.kt
@@ -0,0 +1,14 @@
+//Esperanza Macias 
+//February 7, 2023
+
+fun main() 
+{
+println("   *")
+println("  ***")
+println(" *****")
+println("*******")
+println(" *****")
+println("  ***")
+println("   *")
+
+}
