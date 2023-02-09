c552ca0df97d9835e9c88a893a053afaa94910a6
Esperanza Macias <123098144+karenesper@users.noreply.github.com>
2023-02-09 08:57:11 -0600

    Create string.Mani.kt

---
+++ unit02/string.Mani.kt
@@ -0,0 +1,13 @@
+//Esperanza Macias 
+//February 9, 2023
+
+fun main() 
+{
+var city = "zacatecas city"
+println("The length of the city name " + city.length)
+println(city.toUpperCase())
+println(city.toLowerCase())
+print(city[0])
+
+
+}
