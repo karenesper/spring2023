e4b0f82e79bff07d32b52b96a276eb56e1336e4f
Esperanza Macias <123098144+karenesper@users.noreply.github.com>
2023-02-09 11:36:11 -0600

    Create salesPrediction.kt

---
+++ unit02/salesPrediction.kt
@@ -0,0 +1,12 @@
+//Esperanza Macias 
+//February 9, 2023
+
+fun main() 
+{
+var prct = 0.62;
+val cSales = 4600000;
+var sales = cSales * prct;
+    
+ print("The East Coast division is predicted to generate $sales")
+ 
+}
