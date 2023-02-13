4b56a218af8c38b1a030e311095d8134a592da9c
Esperanza Macias <123098144+karenesper@users.noreply.github.com>
2023-02-12 23:33:36 -0600

    Create stringMani2.kt

---
+++ unit02/stringMani2.kt
@@ -0,0 +1,13 @@
+// Esperanza Macias
+// Feb 12 2023
+
+fun main(args: Array<String>) {
+    println("Enter a String: ")
+    val input = readLine()!!
+    val index = 0
+    val ch = input.get(index)
+    println(input.length)
+    println(input.toUpperCase())
+    println(input.toLowerCase())
+    println(input.get(index))
+}
