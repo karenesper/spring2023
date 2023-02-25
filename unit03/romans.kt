42f1912b035aafb16c75e6372e0878fb73c56f02
Esperanza Macias <123098144+karenesper@users.noreply.github.com>
2023-02-25 00:04:36 -0600

    Update romans.kt

--- unit03/romans.kt
+++ unit03/romans.kt
@@ -1,1 +1,13 @@
-2lol
+// Esperanza Macias
+// Feb 25 2023
+fun main() {
+    print("Enter a number between 1 and 10: ")
+    val number = readLine()?.toIntOrNull()
+
+    if (number != null && number in 1..10) {
+        val romanNumerals = arrayOf("", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X")
+        println("The Roman numeral equivalent of $number is ${romanNumerals[number]}")
+    } else {
+        println("Error: Please enter a valid number between 1 and 10")
+    }
+}
