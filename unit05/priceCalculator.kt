6dbc64415e7060305f18e45ecead878e035cd149
Esperanza Macias <123098144+karenesper@users.noreply.github.com>
2023-04-23 22:52:34 -0500

    Update priceCalculator.kt

--- unit05/priceCalculator.kt
+++ unit05/priceCalculator.kt
@@ -1,6 +1,9 @@
 fun calculateRetail(wholesaleCost: Double, markupPercentage: Double): Double {
   
     val markupAmount = wholesaleCost * (markupPercentage / 100)
+    // The retail price is calculated by adding the markup amount to the wholesale cost
+    
+    return wholesaleCost + markupAmount
     return wholesaleCost + markupAmount
 }
 
@@ -11,6 +14,8 @@ fun main() {
     
     print("Enter the markup percentage of the item: ")
     val markupPercentage = readLine()!!.toDouble()
+    
+    // Calls the calculateRetail function with the wholesale cost and markup percentage as arguments and stores the result in the retailPrice variable
     
     val retailPrice = calculateRetail(wholesaleCost, markupPercentage)
     println("The item's retail price is: $retailPrice")
