bb3633729695471805b1209f885c1f93d014e6d2
Esperanza Macias <123098144+karenesper@users.noreply.github.com>
2023-04-23 23:36:30 -0500

    Update hello.kt

--- week01/hello.kt
+++ week01/hello.kt
@@ -1,8 +1,9 @@
 /**
  * My first Kotlin file / Esperanza Macias
  */
- 
+
  fun main() {
- println(Hello, World!")
- println("I am learning Kotlin.")
- println("Not so bad for my first time.")
+  println("Hello World!")
+  println("I am learning Kotlin for the first time.")
+  println("It is so interesting!")
+}
