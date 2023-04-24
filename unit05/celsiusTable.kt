635765d69c258095886528c10a874d5bf9b78878
Esperanza Macias <123098144+karenesper@users.noreply.github.com>
2023-04-23 23:18:38 -0500

    Update celsiusTable.kt

--- unit05/celsiusTable.kt
+++ unit05/celsiusTable.kt
@@ -2,9 +2,11 @@ fun main() {
   
     println("Fahrenheit\tCelsius")
     
+    // Loop through the Fahrenheit temperatures from 0 to 20
     for (fahrenheit in 0..20) {
       
-        val celsius = celsius(fahrenheit.toDouble())
+      // Convert the current Fahrenheit temperature to Celsius using the celsius() function
+      val celsius = celsius(fahrenheit.toDouble())
         
         println("$fahrenheit\t\t${"%.2f".format(celsius)}")
     }
