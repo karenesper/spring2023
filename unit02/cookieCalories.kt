ca37c7cbdee09e57d15f40f38e93a6fbca91779c
Esperanza Macias <123098144+karenesper@users.noreply.github.com>
2023-04-23 23:39:10 -0500

    Update cookieCalories.kt

--- unit02/cookieCalories.kt
+++ unit02/cookieCalories.kt
@@ -1,10 +1,17 @@
 // Esperanza Macias
 // Feb 12 3023 import java.util.Scanner  
 
-fun main(args: Array<String>) {  
-    val read = Scanner(System.`in`)  
-    println("Enter number of cookies eaten:")  
-    var numOfCookies= read.nextInt()  
-    val total = numOfCookies* 75
-    println("Your calorie intake was: "+ total)  
+fun main() {
+    val cookiesPerBag = 40
+    val servingsPerBag = 10
+    val caloriesPerServing = 300
+    
+    print("Enter the number of cookies you ate: ")
+    val cookiesAte = readLine()!!.toInt()
+    
+    val servingsAte = cookiesAte.toDouble() / cookiesPerBag * servingsPerBag
+    val totalCalories = servingsAte * caloriesPerServing
+    
+    println("You ate $servingsAte servings, which is a total of $totalCalories calories.")
 }
+
