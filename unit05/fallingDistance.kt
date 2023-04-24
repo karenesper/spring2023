15c318db213dafcd5897a7c1a698806e6f631177
Esperanza Macias <123098144+karenesper@users.noreply.github.com>
2023-04-23 23:20:59 -0500

    Update fallingDistance.kt

--- unit05/fallingDistance.kt
+++ unit05/fallingDistance.kt
@@ -1,3 +1,4 @@
+// The function calculates the distance an object falls during a given time in seconds
 fun fallingDistance(time: Double): Double {
   
     val g = 9.8
@@ -8,9 +9,11 @@ fun fallingDistance(time: Double): Double {
 
 fun main() {
   
+    // Loop from 1 to 10 seconds
     for (i in 1..10) {
       
-        val time = i.toDouble()
+        // Convert loop variable to Double for time parameter
+      val time = i.toDouble()
         val distance = fallingDistance(time)
         println("After $time seconds, the object has fallen $distance meters.")
         
